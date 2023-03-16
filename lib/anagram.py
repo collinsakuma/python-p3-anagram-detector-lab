aed40fdb68a269b5c26685f0855e0669998c564c
Collin Sakuma <collinmsakuma@gmail.com>
2023-03-16 09:22:47 -0700

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        anagram_list = []
+        for word in word_list:
+            if sorted([letter for letter in word]) == sorted([letter for letter in self.word]):
+                anagram_list.append(word)
+            else:
+                None
+        if len(anagram_list) == 0:
+            return []
+        else:
+            return anagram_list
+
+
+Anagram("enlist").match(["listen", "silent", "hippopotamus"])
